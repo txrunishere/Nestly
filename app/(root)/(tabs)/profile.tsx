b2f988cf7bde585ed67ff66a950c821a7b7f9529
txrunishere <tsoni9286@gmail.com>
2026-04-29 12:26:13 +0530

    init project setup nativewind and create basic tab screens

---
+++ app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function profile() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Profile Screen</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
